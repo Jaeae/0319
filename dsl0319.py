51bcb82e3cde528af09c0feac703d72ebaef96d5
Jaeae <sn.zzz@yonsei.ac.kr>
2023-03-19 15:34:29 +0900

    add 3 people

---
+++ dsl0319.py
@@ -0,0 +1,16 @@
+class dsl(object):
+    def __init__(self, name):
+        self.name = name
+    def whois(self):
+        if self.name == 'soeun':
+            print(f"{self.name} is vice president")
+        elif self.name == 'gunwoo':
+            print(f"{self.name} is president")
+        elif self.name == 'chawon':
+            print(f"{self.name} is hongboking")
+
+        else:
+            print(f"{self.name}이 누구")
+
+
+        
